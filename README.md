799681f19abd522c71434f1d240428c923b17a56
Oon Chu Lip <oonchulip97@yahoo.com>
2019-09-22 22:38:03 -0700

    [major] Add setup file and README

---
+++ README.md
@@ -0,0 +1,16 @@
+# **ZSH Setup**
+
+This repository allows you to setup ZSH with my personal configuration file -- hassle free!
+
+## **How to run**
+
+Run the following in your terminal:
+
+```
+cd ~
+git clone https://github.com/oonchulip97/zsh_setup.git
+cd ~/zsh_setup
+./zsh_setup.sh
+```
+
+Have fun terminal-ing :)
---
+++ zsh_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install zsh and oh-my-zsh
+apt install zsh
+chsh -s $(which zsh)
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+# Install zsh plugins
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf ${ZSH}/custom/plugins/fzf/install --bin
+git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
+
+sed -i "s/plugins=(git)/plugins=(git\n
+         fast-syntax-highlighting\n
+         zsh-autosuggestions\n
+         history-substring-search\n
+         fzf-zsh\n
+         colored-man-pages)/" $HOME/.zshrc
