2c40724f6f6346f284957f519e9d3a02efcd3305
Oon Chu Lip <oonchulip97@outlook.com>
2022-05-25 17:34:56 -0700

    [major] Change syntax highlighting repository

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -6,17 +6,17 @@ sudo apt install curl
 # Install zsh and oh-my-zsh
 sudo apt install zsh
 chsh -s $(which zsh) $(whoami)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install zsh plugins
-git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 sed -i "s/plugins=(git)/plugins=(git\n\
-         fast-syntax-highlighting\n\
+         F-Sy-H\n\
          zsh-autosuggestions\n\
          history-substring-search\n\
          fzf\n\
